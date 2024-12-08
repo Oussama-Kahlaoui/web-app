ed2f6b15021e0b7e1c0e8e84be9937da0b422309
Oussama-Kahlaoui <ousama.kahlaoui@gmail.com>
2024-12-08 15:42:21 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// var nameF = "Oussama";
+// let lastN = "Kahlaoui";
+// const Cin = "HH34740";
+
+
+
+// nameF="Othman";
+
+// lastN= "Kah";
+// // Cin = "lhwa"
+
+// console.log(nameF);
+// console.log(lastN);
+// console.log(Cin);
+// console.log(nameF)
+
+
+var  firstname = "Hello";
+const  lastname = "Hello";
+let  position = "Hello";
+
+// ------------------------------------------
+
+  firstname = "Hi";
+//   lastname = "ssalam";
+  position = "ooo";
+
+
+
+
+
+console.log(firstname);
+console.log(lastname);
+console.log(position);
